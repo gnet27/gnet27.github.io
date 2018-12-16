409714a8502d04d5ba3196cd4a8150bc12306822
gnet27 <36115382+gnet27@users.noreply.github.com>
2018-12-15 22:27:51 -0500

    Create index.js

---
+++ 404/js/index.js
@@ -0,0 +1,3 @@
+// Inspired by some other person's idea, but repurposed and reimplemented as a 404 page for my personal website.
+// See it live here:
+// https://themisec.com/this-page-doesnt-exist
